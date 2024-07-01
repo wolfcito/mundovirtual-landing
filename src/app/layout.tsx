b3be2d96c4b0fa1ba798e849dd808445c37161f6
Luis Fernando <guffenix@gmail.com>
2024-06-30 23:10:16 -0500

    feat: landing updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 import './globals.css'
 
 import LocalFont from 'next/font/local'
 
 export const metadata: Metadata = {
   title: {
-    default: 'modespray.xyz',
-    template: '%s | modespray.xyz',
+    default: 'mundovirtual.xyz',
+    template: '%s | mundovirtual.xyz',
   },
-  description: 'Send Tokens to Multiple Addresses in One Click',
+  description:
+    'MundoVirtual ayuda a convertir ideas en realidades a través de asesorías, prototipos y desarrollo de MVP.',
   openGraph: {
-    title: 'modespray.xyz',
-    description: 'Send Tokens to Multiple Addresses in One Click',
-    url: 'https://modespray.xyz',
-    siteName: 'modespray.xyz',
+    title: 'mundovirtual.xyz',
+    description:
+      'MundoVirtual ayuda a convertir ideas en realidades a través de asesorías, prototipos y desarrollo de MVP.',
+    url: 'https://mundovirtual.xyz',
+    siteName: 'mundovirtual.xyz',
     images: [
       {
-        url: 'https://modespray.xyz/og.png',
+        url: 'https://mundovirtual.xyz/og.png',
         width: 1920,
         height: 1080,
       },
@@ -37,7 +39,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: 'ModeSpray',
+    title: 'mundovirtual',
     card: 'summary_large_image',
   },
   icons: {
@@ -45,14 +47,9 @@ export const metadata: Metadata = {
   },
 }
 
-const inter = Inter({
+const inter = Nunito({
   subsets: ['latin'],
   variable: '--font-inter',
-})
-
-const calSans = LocalFont({
-  src: '../../public/fonts/CalSans-SemiBold.ttf',
-  variable: '--font-calsans',
 })
 
 export default function RootLayout({
@@ -61,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={[inter.variable, calSans.variable].join(' ')}>
+    <html lang="en" className={inter.variable}>
       <body
         className={`bg-black ${
           process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined
